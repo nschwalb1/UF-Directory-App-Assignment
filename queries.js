9c5d7b4147d2bdb801148c63d4eaad85a53c7eb6
nschwalb1 <neilschwalb@gmail.com>
2017-09-05 07:24:59 -0400

    Compeleted mongoose queries

--- queries.js
+++ queries.js
@@ -1,12 +1,26 @@
 /* Fill out these functions using Mongoose queries*/
 
 var findLibraryWest = function() {
+    Listing.find({name:'Library West'}, function(err, listing) {
+        if(err) throw err;
+
+        console.log(listing);
+    })
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
 };
 var removeCable = function() {
+    Listing.find({code: 'CABL'}, function(err, listing) {
+        if (err) throw err;
+
+        listing.remove(function(err) {
+            if(err) throw err;
+
+            console.log(listing + ' Successfully deleted');
+        })
+    })
   /*
     Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
@@ -14,12 +28,22 @@ var removeCable = function() {
    */
 };
 var updatePhelpsMemorial = function() {
+    Listing.findOneAndUpdate({code: 'PHL'}, {address: '102 Phelps Lab, Gainesville, FL 32611'}, function(err, listing) {
+        if (err) throw err;
+
+        console.log(listing);
+    })
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
 };
 var retrieveAllListings = function() {
+    Listing.find({}, function(err, listings) {
+        if (err) throw err;
+
+        console.log(listings);
+    })
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
